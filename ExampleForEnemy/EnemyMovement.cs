1179d8fc60621dda9b0fffbdd44cd3f3c7f84846
FloloGames <60265085+FloloGames@users.noreply.github.com>
2021-04-21 16:50:08 +0200

    Example Script for Enemy

---
+++ ExampleForEnemy/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private Transform targetPos;
+
+    [SerializeField] private float moveSpeed = 1000f;
+
+    private Pathfinding pathfinder;
+
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        pathfinder = GetComponent<Pathfinding>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        pathfinder.FindPath(transform.position, targetPos.position);
+    }
+    private void FixedUpdate()
+    {
+        Vector2 dir = pathfinder.GetVelocityToNextNode(transform.position);
+
+        rb2d.AddForce(dir * moveSpeed * Time.deltaTime);
+    }
+}
