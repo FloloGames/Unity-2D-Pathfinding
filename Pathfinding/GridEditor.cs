440db26df33302a134ea8eba875b81b561dae2c9
FloloGames <60265085+FloloGames@users.noreply.github.com>
2021-04-21 16:48:02 +0200

    Update

---
+++ Pathfinding/GridEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathfindingGrid))]
+public class GridEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        PathfindingGrid myScript = (PathfindingGrid)target;
+        if (GUILayout.Button("CREATE GRID"))
+        {
+            myScript.CreateGrid();
+        }
+        if (GUILayout.Button("DESTROY GRID"))
+        {
+            myScript.DestroyGrid();
+        }
+        if (GUILayout.Button("SHOW/HIDE GRID"))
+        {
+            myScript.ShowHideGrid();
+        }
+    }
+}
